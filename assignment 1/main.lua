e789313e1f9896bf2b2003f28687d379494894da
SOPHIE ANGELA VILLALBA <72681828+sophie-angela-villalba@users.noreply.github.com>
2020-11-20 18:15:57 +0800

    bird 0 update

--- assignment 1/main.lua
+++ assignment 1/main.lua
@@ -6,16 +6,16 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
-local background = love.graphics.newImage('background,png')
-local background = love.graphics.newImage('ground.png')
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
 
 function love.load()
-    
+
     love.graphics.setDefaultFilter('nearest','nearest')
 
     love.window.setTitle('Dino Bird')
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT){
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
@@ -33,7 +33,9 @@ function love.keypressed(key)
 end
 
 function love.draw()
+
     push:start()
+    
     love.graphics.draw(background, 0, 0)
     love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
     push:finish()
